2aa88ff6b9cd20d6d74499731f24657f78ca2358
saif0110 <khanmohdtauheed4@gmail.com>
2024-12-31 18:40:07 +0530

    file changed

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const readline = require('readline');
+const path = require('path');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,6 +10,24 @@ const rl = readline.createInterface({
 rl.question('Enter the filename: ', (filename) => {
   rl.question('Enter the word to remove: ', (word) => {
     // TODO: Implement this function
+    const filePath = path.join(__dirname, filename);
+    fs.readFile(filePath, (err, data) => {
+      if(err){
+        throw new Error('Error while reading to file');
+        console.log(err)
+        return;
+      }
+      const regex = new RegExp(`\\b${word}\\b`, 'g');
+      const updatedContent = data.toString().replace(regex, '');
+      fs.writeFile(filePath, updatedContent, (err)=>{
+        if(err){
+          throw new Error('Error while writing to file');
+          console.log(err)
+          return;
+        }
+        console.log('updating file is done successfully')
+      })
+    })
     rl.close();
   });
 });
